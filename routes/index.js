4d5e23c81e63d1180b9f0a3be644f9c770ba2f50
Jarvis555 <jarvisjackson591@gmail.com>
2020-02-10 08:04:49 -0600

    .

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Jackson Sport Store' });
+router.get('/', function (req, res, next) {
+
+  res.render('index', {
+    title: 'Jackson Sport Store',
+
+  });
 });
 
 module.exports = router;
--- routes/products.js
+++ routes/products.js
@@ -4,83 +4,90 @@ var router = express.Router();
 //File Upload code with multer
 var multer = require('multer');
 var storage = multer.diskStorage({
-        destination: function (req, file, cb) {
-            cb(null, 'public/images/')
+    destination: function (req, file, cb) {
+        cb(null, 'public/images/')
     },
-        filename: function (req, file, cb) {
-            cb(null, file.originalname)
+    filename: function (req, file, cb) {
+        cb(null, file.originalname)
     }
 });
 
-var upload = multer({ storage: storage });  
+var upload = multer({
+    storage: storage
+});
 
 
 //Main products page
-router.get('/',function(req, res, next) {
-    res.render('products', {products: myProducts})
+router.get('/', function (req, res, next) {
+
+    res.render('products', {
+        products: myProducts
+    })
 })
 
 
 //Direct User to Create Product Form
-router.get('/product-create',(req, res, next)=> {
+router.get('/product-create', (req, res, next) => {
     res.render('product-create')
 })
 
 //Direct User to Edit Product Form
-router.get('/product-edit/:id',(req, res, next)=> {
+router.get('/product-edit/:id', (req, res, next) => {
     let prodId = req.params.id | 0;
-    let product = myProducts.find(product=>product.id == prodId);
-    res.render('product-edit',{ product: product});
+    let product = myProducts.find(product => product.id == prodId);
+    res.render('product-edit', {
+        product: product
+    });
 });
 
 //Post New Product
-router.post('/product-create', upload.single('imageUpload'), (req,res,next)=>{
+router.post('/product-create', upload.single('imageUpload'), (req, res, next) => {
     myProducts.push({
-       id: req.body.id | 0,
-       name: req.body.name,
-       price: req.body.price,
-       size: req.body.size,
-       image_path: '/images/'+req.file.originalname
+        id: req.body.id | 0,
+        name: req.body.name,
+        price: req.body.price,
+        size: req.body.size,
+        image_path: '/images/' + req.file.originalname
     })
     res.redirect('/products')
 })
 
 //Edit Product
-router.post('/product-edit/:id', upload.single('imageUpload'), (req,res,next)=>{
-    
+router.post('/product-edit/:id', upload.single('imageUpload'), (req, res, next) => {
+
     //find product in array
     let prodId = req.params.id | 0;
-    productToEdit = myProducts.find(product=> product.id === prodId);
-    
+    productToEdit = myProducts.find(product => product.id === prodId);
+
     productToEdit.id = req.body.id | 0;
-    productToEdit.name =  req.body.name;
+    productToEdit.name = req.body.name;
     productToEdit.price = req.body.price;
     productToEdit.size = req.body.size;
-    let imgPath = req.file == undefined ? req.body.image_path : '/images/'+req.file.originalname ;
+    let imgPath = req.file == undefined ? req.body.image_path : '/images/' + req.file.originalname;
     productToEdit.image_path = imgPath;
 
     res.redirect('/products')
 })
 
-router.get('/product-delete/:id', (req,res,next)=>{
-    
-    //find product in array
+router.get('/product-delete/:id', (req, res, next) => {
     let prodId = req.params.id | 0;
-    myProducts.splice(myProducts.findIndex(product=> product.id === prodId), 1);
-    
+    let productDelete = myProducts.splice(myProducts.findIndex(product => product.id === prodId), 1);
 
-    res.redirect('/products')
+    res.render('deleted-product', {
+        product: productDelete[0]
+    })
 })
-
 //Product details page
-router.get('/:id',function(req, res, next) {
+router.get('/:id', function (req, res, next) {
     let prodId = req.params.id | 0;
-    let prod = myProducts.find(product=> product.id === prodId);
-    res.render('product-detail', { product: prod })
+    let prod = myProducts.find(product => product.id === prodId);
+    res.render('product-detail', {
+        product: prod
+    })
 })
 
-class Product{
-    constructor(id,name,price,size,image_path){
+class Product {
+    constructor(id, name, price, size, image_path) {
         this.id = id
         this.name = name
         this.price = price
@@ -91,11 +98,11 @@ class Product{
 
 let myProducts = []
 myProducts.push(
-    new Product(1,'Helmet',3.99,'M','/images/baseballhelmet.jfif'),
-    new Product(2,'Bat',75.99,'34"','/images/bat.jfif'),
-    new Product(3,'Glove',35.00,'M','/images/glove.jfif'),
-    new Product(4,'Baseball',15.99,'M','/images/baseball.jfif'),
-    new Product(5,'Baseball Cleats',22.99,'M','/images/cleats.jfif')
+    new Product(1, 'Helmet', 3.99, 'M', '/images/baseballhelmet.jfif'),
+    new Product(2, 'Bat', 75.99, '34"', '/images/bat.jfif'),
+    new Product(3, 'Glove', 35.00, 'M', '/images/glove.jfif'),
+    new Product(4, 'Baseball', 15.99, 'M', '/images/baseball.jfif'),
+    new Product(5, 'Baseball Cleats', 22.99, 'M', '/images/cleats.jfif')
 
 )
 
